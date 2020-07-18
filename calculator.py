a528ecb44e066892c4a503ac8493563f5b1e339d
Constance Huang <constance.huang98@gmail.com>
2020-07-18 23:34:05 +0000

    Added quit feature

--- calculator.py
+++ calculator.py
@@ -5,3 +5,12 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+
+while True:
+    input_string = input("Enter an Operator and Number(s): ")
+    input_string = input_string.split(' ')
+    if input_string[0] == "q":
+        break
+    else:
+        pass
+
