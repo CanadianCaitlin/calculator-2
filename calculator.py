1fdb38c759a613888f6109868061879591f6f59d
Constance Huang <constance.huang98@gmail.com>
2020-07-19 00:26:01 +0000

    Completed Code

--- calculator.py
+++ calculator.py
@@ -3,7 +3,6 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                        power, mod)
 
 
-# Replace this with your code
 def calculator2():
     while True:
         input_string = input("Enter an Operator and Number(s): ")
@@ -13,17 +12,36 @@ def calculator2():
         if operator == "q":
             break
         else:
-            num1= int(input_string[1])
 
+            num1= float(input_string[1])
             if len(input_string) ==3:
-                num2= int(input_string[2])
-    
+                num2= float(input_string[2])
+
+            #add
             if operator == "+":
                 print(num1+num2)
+            #substract
+            elif operator == "-":
+                print(num1-num2)
+            #multiply
+            elif operator == "*":
+                print(num1*num2)
+            #divide
+            elif operator == "/":
+                print(num1/num2)
+            #cube
+            elif operator == "cube":
+                print(num1**3)
+            #power
+            elif operator == "pow":
+                print(num1**num2)
+            #mod
+            elif operator == "mod":
+                print(num1 % num2)
+            #square
             elif operator == "square":
                 print(num1**2)
-
-
+            
 calculator2()
         
 
