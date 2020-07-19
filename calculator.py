c7bb0dd5922d2d333d50c8c33b226410abc06964
Constance Huang <constance.huang98@gmail.com>
2020-07-19 00:14:37 +0000

    Added addition and square feature

--- calculator.py
+++ calculator.py
@@ -1,23 +1,33 @@
 """CLI application for a prefix-notation calculator."""
-
 from arithmetic import (add, subtract, multiply, divide, square, cube,
-                        power, mod, )
+                       power, mod)
 
 
 # Replace this with your code
+def calculator2():
+    while True:
+        input_string = input("Enter an Operator and Number(s): ")
+        input_string = input_string.split(' ')
+        operator= input_string[0]
 
-while True:
-    input_string = input("Enter an Operator and Number(s): ")
-    input_string = input_string.split(' ')
-    operator= input_string[0]
+        if operator == "q":
+            break
+        else:
+            num1= int(input_string[1])
+
+            if len(input_string) ==3:
+                num2= int(input_string[2])
+    
+            if operator == "+":
+                print(num1+num2)
+            elif operator == "square":
+                print(num1**2)
 
 
-    if operator == "q":
-        break
-    else:
-        num1= int(input_string[1])
-        if len(input_string) ==3:
-            num2= int(input_string[2])
+calculator2()
+        
+
+        
 
   
 
